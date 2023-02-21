3b0c8a8896ea297adf37dcf660a0b656bace1f8d
Dunned <eduardo.jauregui1@unmsm.edu.pe>
2023-02-20 20:26:14 -0500

    push 7

--- Knight.java
+++ Knight.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
  * @author Myles David II (davidmm2)
  * @author Danielle Bushrow (dbushrow)
  * @version 2010.11.17
- hola*/
+ holas*/
 public class Knight
     extends ChessGamePiece{
     /**
